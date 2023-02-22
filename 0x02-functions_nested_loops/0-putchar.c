68a3a817a3d78aa4892d9e2da9a0e1a80884e13c
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-02-22 03:34:14 +0000

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = "_putchar";
+	_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
