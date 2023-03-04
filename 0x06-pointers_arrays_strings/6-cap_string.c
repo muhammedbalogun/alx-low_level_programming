cac8902c7363ebc741925d189690c1acccb41cfb
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-04 03:13:41 +0000

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ char *cap_string(char *str)
 		str[i - 1] == '?' ||  str[i - 1] == '"' ||
 		str[i - 1] == '(' ||  str[i - 1] == ')' ||
 		str[i - 1] == '{' ||  str[i - 1] == '}' ||
-		str[i - 1] == '.') && (str[i] >= 'a' && s[i] <= 'z'))
+		str[i - 1] == '.') && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			str[i] = str[i] - 32;
 		}
