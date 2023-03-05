a5e4231197b058f2fadc0e202edbb444a5edaa52
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-05 15:56:08 +0000

    program that prints all the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+ * main - Entery point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch < '10'; ch++)
+		putchar(ch);
+
+	for (ch = 'a'; ch < 'g'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
