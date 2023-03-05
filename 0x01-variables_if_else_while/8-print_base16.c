e0e6930d60961556bc91b2430225b51f3d2c3f57
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-05 16:06:31 +0000

    program that prints all the numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,15 @@ int main(void)
 {
 	char ch;
 
-	for (ch = '0'; ch < '10'; ch++)
+	for (ch = '0'; ch <= '9'; ch++)
+	{
 		putchar(ch);
+	}
 
 	for (ch = 'a'; ch < 'g'; ch++)
+	{
 		putchar(ch);
+	}
 
 	putchar('\n');
 
