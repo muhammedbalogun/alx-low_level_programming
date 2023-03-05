5b435968911272012c412aafbfeec60983f06284
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-05 17:31:12 +0000

    print all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+
+		if (ch != '9')
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
