80bfc457985f7f23014de4156e19b4b14fe601bd
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-06 04:55:58 +0000

    prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts - print a string followed by a new line
+ * @s: pointer to a string to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
