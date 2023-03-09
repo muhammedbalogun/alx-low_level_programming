acb24464e452598eda68258760dbc45c3543e16b
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-09 03:18:58 +0000

    function that returns x to power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that return x to power of y
+ * @x: base number
+ * @y: power
+ *
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return ((x * x) * _pow_recursion(x, y - 1));
+}
