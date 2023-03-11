033b103ebb318ad34299fed1237b2be03cfdfa98
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-11 04:44:18 +0000

    program that print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints its name
+ * @argc: arg size
+ * @argv: arg value
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
