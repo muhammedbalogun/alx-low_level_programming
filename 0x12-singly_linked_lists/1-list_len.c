55d85fa7f740a0f34228fe065e4995ab07a4d344
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-03-31 04:15:56 +0000

    fuctn that finds num of element in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - finds the number of elements in a list
+ * @h: linked list_t list
+ * @elements: a counter var for size_t (nodes)
+ *
+ * Return: The number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
