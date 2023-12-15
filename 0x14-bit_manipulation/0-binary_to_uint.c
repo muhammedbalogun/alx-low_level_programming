4557ac21ed20062d9f6906d3988d01eadcdda6c8
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-12-15 04:58:08 +0000

    fuction that convert binary to un-int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int i = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
 	{
 		num <<= 1;
 		num += b[i] - '0';
-		i++;
 	}
 	return (num);
 }
