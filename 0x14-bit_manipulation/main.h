c82734a3271264f93f2c688766a42bc08ce76ef2
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-12-15 00:56:25 +0000

    modified header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -9,4 +9,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
-#endif /*MAIN.H*/
+#endif /*MAIN_H*/
