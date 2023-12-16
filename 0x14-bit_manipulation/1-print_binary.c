888294702c3a84f38404b9e764a46843eaccb911
muhammedbalogun <muhammed.balogun123@gmail.com>
2023-12-16 04:32:12 +0000

    function that print binary number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_binary - function that print binary rep. of a number
@@ -10,16 +10,20 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int num = 0;
-	int i;
+	unsigned long int num;
 
-	if (b == NULL)
+	if (n == 0)
 		return (0);
 
-	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
+	num = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	while (num > 0)
 	{
-		num <<= 1;
-		num += b[i] - '0';
+		if (n & num)
+			_putchar('0');
+		else
+			_putchar('0');
+		num >>= 1;
 	}
 	return (num);
 }
