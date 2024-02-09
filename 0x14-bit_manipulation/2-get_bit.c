d5eccc887624060275f071338c597df4e5fc0dec
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-02-09 06:35:00 +0000

    added task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at an index
+ * @n: nunber variable
+ * @index: index to return
+ *
+ * Return: value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int value;
+
+	if ()
+		return;
+}
