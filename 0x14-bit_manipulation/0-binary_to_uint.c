124657fe200ed76a7b0a626b7ef708224bba6e59
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-02-09 04:54:49 +0000

    updated task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
+	for (i = 0; b[i]; i++)
 	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+
 		num <<= 1;
 		num += b[i] - '0';
 	}
